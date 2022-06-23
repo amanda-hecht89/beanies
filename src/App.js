81f2eb1f87f4ead7cdef6d714dcc57eba97e772b
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-23 08:10:20 -0600

    App js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  NavLink,
 } from 'react-router-dom';
 import BeaniesPage from './BeaniesPage';
 import BeanieDetail from './BeanieDetail';
@@ -12,11 +13,11 @@ export default function App() {
     <Router>
       <div className='App'>
         <Switch>
-          <Route>
-            {/* this home page route should list all the beanies */}
+          <Route exact path="/">
+            <BeaniesPage />
           </Route>
-          <Route> 
-            {/* this route should point to a particulat beanie baby by id and render that specific BeanieDetail page */}
+          <Route exact path="/beanies/:id"> 
+            <BeanieDetail />
           </Route>
         </Switch>
       </div>

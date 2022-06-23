9ad74f998b67f5bd87e0c212c27eb48ce80e52da
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-23 09:35:32 -0600

    too broke to go on

--- src/BeaniesPage.js
+++ src/BeaniesPage.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { getBeanieBabies } from './services/fetch-utils';
 import BeaniesList from './BeaniesList';
+import Pagination from './Pagination';
 
 function App() {
   const [beanieBabies, setBeanieBabies] = useState([]);
@@ -18,20 +19,18 @@ function App() {
     }
 
     fetch();
-  }, []); // what can you do with this array to trigger a fetch every time the page changes?
+  }, [page]); // what can you do with this array to trigger a fetch every time the page changes?
+
 
   return (
-    <>
+    <Pagination
       <h2>Current Page {page}</h2>
       <div className='buttons'>
-        {/* on click, this button should decrement the page in state  */}
-        {/* also, disable this button when you are on the first page */}
-        <button>Previous Page</button>
-        {/* on click, this button should increment the page in state  */}
-        <button >Next Page</button>
+        <button>Previous Page={page}</button>
+        <button >Next Page={setPage}</button> 
       </div>
-      {/* pass the beanie babies into the BeaniesList component */}
-    </>
+    />  
+    <BeaniesList beanieBabies={beanieBabies} />
   );
 }
 

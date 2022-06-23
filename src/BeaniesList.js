6bdffc45dbe1c6cc1a7e13407fce33c8880a5376
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-23 08:47:32 -0600

    beanie list js

--- src/BeaniesList.js
+++ src/BeaniesList.js
@@ -1,8 +1,17 @@
 import BeanieBaby from './BeanieBaby.js';
+import { Link } from 'react-router-dom';
 
 export default function BeaniesList({ beanieBabies }) {
   return (
-    <div className='beanies'>
-      {/* render out the beanie babies as a list */}
+    <div className='beanies'>{
+      beanieBabies.map((beanieBaby, i) =>
+        <Link key={beanieBaby.title + i + beanieBaby.id} to={`/beanies/${`/beanieBaby.id`}`}>
+          <div className='beanies'>
+            <h3>{beanieBaby.title}</h3>
+            <p>{beanieBaby.astrosign}</p>
+            <img src={beanieBaby.image} />
+          </div>
+        </Link>)
+    }
     </div>);
 }

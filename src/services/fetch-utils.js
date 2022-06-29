943ab1180864e90d1c0d6f53942495a644dda308
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-29 16:09:13 -0600

    fixed fetch utils

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -6,7 +6,7 @@ export async function getBeanieBabies(from = 0, to = 30) {
     .select()
     .range(from, to);
 
-  return checkError(response);
+  return response.body;
 }
 
 export async function getSingleBeanie(id) {
@@ -16,6 +16,6 @@ export async function getSingleBeanie(id) {
     .match({ id })
     .single();
 
-  return checkError(response);
+  return response.body;
 
 }
